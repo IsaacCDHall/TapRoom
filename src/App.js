9d3b2fdf4b4f422494353485e3c829b9bfdc373b
Isaac Hall <isaacdehall@gmail.com>
2019-07-12 09:03:11 -0700

    add routing and beer component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
 import React, {Component} from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header.js'
 import MainPage from './components/MainPage.js'
 import beerList from './models/beerList.js'
+import Beer from './components/Beer.js'
 class App extends Component {
   constructor(){
     super()
     this.state= {
       beerList: beerList
     }
+    this.handleDeleteKeg = this.handleDeleteKeg.bind(this);
+    this.handleSellPint = this.handleSellPint.bind(this);
   }
   render(){
     return (
       <div className="App">
+        <style jsx global>{`
+          *{
+            -webkit-box-sizing: border-box;
+            -moz-box-sizing: border-box;
+            box-sizing: border-box;
+          }`}
+        </style>
         <Header/>
-        <MainPage/>
+        <Switch>
+          <Route exact path ='/' component={MainPage}/>
+          <Route exact path ='/Beers' component={() => <Beer beerList={this.state.beerList} handleSellPint={this.handleSellPint} handleDeleteKeg={this.handleDeleteKeg}/>}/>
+        </Switch>
       </div>
+
     );
 
   }
---
+++ src/components/Beer.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+
+class Beer extends Component {
+  render(){
+    return (
+      <div className="">
+        <p>Beer</p>
+      </div>
+    );
+
+  }
+}
+
+export default Beer;
