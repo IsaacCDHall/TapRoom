5486dc603a0abf8b444de1b25c8aaf5d3135df92
Isaac Hall <isaacdehall@gmail.com>
2019-07-12 08:30:45 -0700

    initialize project with simple directory

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+
+class Header extends Component {
+  render(){
+    return (
+      <div className="">
+        <p> test</p>
+      </div>
+    );
+
+  }
+}
+
+export default Header;
---
+++ src/components/MainPage.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+
+class MainPage extends Component {
+  render(){
+    return (
+      <div className="">
+        <p> test 2</p>
+      </div>
+    );
+
+  }
+}
+
+export default MainPage;
